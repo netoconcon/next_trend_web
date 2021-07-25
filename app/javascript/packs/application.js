b3487a65203ee753d981dcfefeacafe7d4b6253f
Neto <concon.neto2@gmail.com>
2021-07-25 00:52:20 -0300

    next trend web

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,10 +25,14 @@ require("channels")
 // External imports
 import "bootstrap";
 
+
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { loadDynamicBannerText } from '../components/banner';
+
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  loadDynamicBannerText()
 });
