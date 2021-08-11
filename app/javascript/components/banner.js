c72725a99486f527c92de2c8aa49b2cd43385a04
Neto <concon.neto2@gmail.com>
2021-08-10 21:43:54 -0300

    fixed typed

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -8,9 +8,33 @@ const loadDynamicBannerText = () => {
     strings: [
     "[Reddit] GME to the moon ...",
     "[Reddit] GME to the moon .",
-    "[Reddit] GME to the moon ...",
-    "[Reddit] GME to the moon ...",
-    "[Reddit] GME to the moon ",
+    "[Reddit] GME to the moon 🟢",
+    "[Reddit] GME to the moon 🟢",
+
+    "[Yahoo] Apple Suppliers Could Benefit From New iPhone Models ...",
+    "[Yahoo] Apple Suppliers Could Benefit From New iPhone Models .",
+    "[Yahoo] Apple Suppliers Could Benefit From New iPhone Models 🟢",
+    "[Yahoo] Apple Suppliers Could Benefit From New iPhone Models 🟢",
+
+    "[Reddit] Hedgefunds and Wallstreet just want to short these companies to zero and make millions ...",
+    "[Reddit] Hedgefunds and Wallstreet just want to short these companies to zero and make millions .",
+    "[Reddit] Hedgefunds and Wallstreet just want to short these companies to zero and make millions 🔴",
+    "[Reddit] Hedgefunds and Wallstreet just want to short these companies to zero and make millions 🔴",
+
+    "[Reddit] You guys are champs. GME... who would have thought a bunch of crazy retards could reach the front page of the New York Times ...",
+    "[Reddit] You guys are champs. GME... who would have thought a bunch of crazy retards could reach the front page of the New York Times .",
+    "[Reddit] You guys are champs. GME... who would have thought a bunch of crazy retards could reach the front page of the New York Times 🔴",
+    "[Reddit] You guys are champs. GME... who would have thought a bunch of crazy retards could reach the front page of the New York Times 🔴",
+
+    "[Bloomberg] Apple Readies New iPhones With Pro-Focused Camera, Video Updates ...",
+    "[Bloomberg] Apple Readies New iPhones With Pro-Focused Camera, Video Updates .",
+    "[Bloomberg] Apple Readies New iPhones With Pro-Focused Camera, Video Updates 🟡",
+    "[Bloomberg] Apple Readies New iPhones With Pro-Focused Camera, Video Updates 🟡",
+
+    "[Reddit] I believe right now is one of those rare opportunities that we all can help and do good. Some of these companies like GME ...",
+    "[Reddit] I believe right now is one of those rare opportunities that we all can help and do good. Some of these companies like GME .",
+    "[Reddit] I believe right now is one of those rare opportunities that we all can help and do good. Some of these companies like GME 🟢",
+    "[Reddit] I believe right now is one of those rare opportunities that we all can help and do good. Some of these companies like GME 🟢",
 
     ],
     typeSpeed: 120,
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   get 'tesla', to: "pages#tesla"
   get 'apple', to: "pages#apple"
+  get 'game', to: "pages#game"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
